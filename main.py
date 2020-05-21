65b2c3073ce143872f3c07c8e49b26746e8e1d15
EzePze <taylorj@macstudent.com.au>
2020-05-22 09:37:00 +1000

    Rebuilt text generation to rely on one model

--- main.py
+++ main.py
@@ -12,41 +12,15 @@ import random
 import sys
 import os
 import hashlib
+import generator
+import fire
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
 def read_data(file_name):
     #open and read text file
     text = open(file_name, encoding='utf-8').read()
     return text.lower()
-
-def generateText():
-    start_index = random.randint(0, len(text) - maxlen - 1)
-    generated = ''
-    sentence = text[start_index: start_index + maxlen]
-    generated += sentence
-    print('----- Generating with seed: "' + sentence + '"')
-    print("""
-    """)
-    sys.stdout.write(generated)
-
-    string_mapped = X[99]
-
-    for i in range(400):
-        x = np.reshape(string_mapped,(1,len(string_mapped), 1))
-        x = x / float(len(chars))
-
-        preds = model.predict(x, verbose=0)[0]
-        next_index = np.argmax(preds)
-        next_char = indices_char[next_index]
-
-        sentence = sentence[1:] + next_char
-
-        sys.stdout.write(next_char)
-        sys.stdout.flush()
-        
-        string_mapped.append(next_index)
-        string_mapped = string_mapped[1:len(string_mapped)]
-    print()
-
 
 def on_epoch_end(epoch, _):
     # Function invoked at end of each epoch. Prints generated text.
@@ -59,6 +33,37 @@ def on_epoch_end(epoch, _):
 
     #generateText()
 
+def get_model_selection():
+    print("Select a model or create a new one:\n\n[0] New Model\n[1] Delete Model")
+    for i in model_dict:
+        print('[%d] %s' % (i, model_dict[i].title()))
+
+    print()
+    model_select = int(input())
+    print()
+    return model_select
+
+def update_dict(list = 'ListOfModels.txt'):
+    data = read_data(list)
+    return dict((i,c) for i,c in enumerate(data.split(), 2))
+
+def create_model(name, data):
+    with open('ListOfModels.txt', 'a') as modelList:
+        modelList.write(name + '\n')
+    with open('%s.txt' % name, 'w') as newData:
+        newData.write(read_data(data))
+    print('\nCreated model \'%s\'\n' % name)
+
+def delete_model(name):
+    oldList = read_data('ListOfModels.txt')
+    oldList = oldList.replace(name,'')
+    with open('ListOfModels.txt', 'w') as newList:
+        newList.write(oldList)
+    if os.path.isfile(name + "Model.h5"):
+        os.remove('%sModel.h5' % name)
+    if os.path.isfile(name + ".txt"):
+        os.remove('%s.txt' % name)
+    print('Deleted model \'%s\'\n' % name)
 
 # def sample(preds, temperature=1.0):
 #     # helper function to sample an index from a probability array
@@ -69,20 +74,37 @@ def on_epoch_end(epoch, _):
 #     probas = np.random.multinomial(1, preds, 1)
 #     return np.argmax(probas)
 
-def create_model(name, data):
-    with open('ListOfModels.txt', 'a') as modelList:
-        modelList.write(name + '\n')
-    with open('%s.txt' % name, 'w') as newData:
-        newData.write(read_data(data))
+# def generateText():
+#     start_index = random.randint(0, len(text) - maxlen - 1)
+#     sentence = input('\nEnter an opening sentence: ').lower() if input('''[1] Enter custom input
+# [2] Use sentence from training data\n\n''') == '1' else text[start_index: start_index + maxlen]
+#     print('----- Generating with seed: "' + sentence + '"')
+#     for diversity in [0.2, 0.5, 1.0, 1.2]:
+#         print("""
+#         """)
+#         print('----- Diversity:', diversity)
+#         print(""" """)
 
-def delete_model(name):
-    oldList = read_data('ListOfModels.txt')
-    oldList.replace(name,'')
-    with open('ListOfModels.txt', 'w') as newList:
-        newList.write(oldList)
-    if os.path.isfile(name + "Model.h5"):
-        os.remove('%sModel.h5' % name)
-    os.remove('%s.txt' % name)
+#         generated = ''
+#         print("""
+#         """)
+#         generated += sentence
+#         sys.stdout.write(generated)
+
+#         for i in range(400):
+#             x_pred = np.zeros((len(chars), maxlen, 1))
+#             for t, char in enumerate(sentence):
+#                 x_pred[t, 0, char_indices[char]] = 1.
+
+#             preds = model.predict(x_pred, verbose=0)[0]
+#             next_index = sample(preds, diversity)
+#             next_char = indices_char[next_index]
+
+#             sentence = sentence[1:] + next_char
+
+#             sys.stdout.write(next_char)
+#             sys.stdout.flush()
+#         print()
 
 print("  _________ __                       ___________    .__  .__                ")
 print(" /   _____//  |_  ___________ ___.__.\__    ___/___ |  | |  |   ___________ ")
@@ -114,36 +136,41 @@ print("")
      #   print("Invalid login. Try again.\n")
 
 #print("Login successful. Welcome, %s.\n" %user)
-listOfModels = read_data('listOfModels.txt')
+model_dict = update_dict()
+model_select = get_model_selection()
 
-model_dict = dict((i,c) for i,c in enumerate(listOfModels.split(), 2))
-print("Select a model or create a new one:\n[0] New Model\n[1] Delete Model")
-for i in model_dict:
-    print('[%d] %s' % (i, model_dict[i].title()))
+while model_select not in range(2, len(model_dict) + 2):
+    if not model_select:
+        name = input('\nEnter the name for your new model: ').lower()
+        data = input('\nEnter the name of the text file to be used for training (needs to be in the /Storyteller folder): ')
+        while not os.path.isfile(data):
+            print('\nInvalid training data. Are you sure the file \'%s\' is in the /Storyteller folder?\n' % data)
+            data = input('\nEnter the name of the text file to be used for training (needs to be in the /Storyteller folder): ')
+        create_model(name, data)
+        model_dict = update_dict()
+        model_select = get_model_selection()
 
-print()
-model_select = int(input())
-chosen_model = 0
+    elif model_select == 1:
+        print('\nWhich model do you wish to delete?\n')
+        for i in model_dict:
+            print('[%d] %s' % (i - 2, model_dict[i].title()))
+        print()
+        name = int(input())
+        if input('Are you sure you wish to delete model \'%s\'? (y/n)\n\n' % model_dict[name + 2]) == 'y':
+            print()
+            delete_model(model_dict[name + 2])
+            model_dict = update_dict()
+            print('\nReturning to model selection screen...\n\n')
+        else:
+            print('Cancelling...\n\nReturning to model selection...\n')
+        model_select = get_model_selection()
+    
+    else:
+        print('Please choose a number in the provided range\n')
+        model_select = get_model_selection()
 
-if not model_select:
-    name = input('\nEnter the name for your new model: '.lower())
-    data = input('\nEnter the name of the text file to be used for training (needs to be in the /Storyteller directory): ')
-    create_model(name, data)
 
-elif model_select == 1:
-    print('\nWhich model do you wish to delete?')
-    for i in model_dict:
-        print('[%d] %s' % (i - 2, model_dict[i].title()))
-    name = int(input())
-    delete_model(model_dict[name + 2])
-
-else:
-    while not chosen_model:
-        try:
-            model_select = int(input())
-            chosen_model = model_dict[model_select]
-        except:
-            print('Please choose a number in the provided range')
+chosen_model = model_dict[model_select]
 
 train = (input("""Select a mode:
 
@@ -227,6 +254,6 @@ if train:
         epochs=100,
         callbacks=[print_callback])
 else:
-    generateText()
+    fire.Fire(generator.interact_model())
 
 
